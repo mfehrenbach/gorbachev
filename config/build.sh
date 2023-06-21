5299f99d3672144053e342758053e0af13db3292
Michael Fehrenbach <mf@michaelfehrenbach.com>
2023-06-21 17:53:14 -0400

    Switch to HTTPS for local dev (thanks @bep).

--- config/build.sh
+++ config/build.sh
@@ -1,6 +1,5 @@
 config=config/_default/hugo.yml
 destination=public
-port=80
 
 rm -rf $destination &&
 
@@ -9,5 +8,3 @@ rm -rf resources &&
 html-beautify --indent-inner-html --extra_liners --no-preserve-newlines --indent-with-tabs --replace "$destination/**/*.html" &&
 css-beautify --space_around_combinator --indent-with-tabs --replace "$destination/**/*.css" &&
 js-beautify --indent-with-tabs --replace "$destination/**/*.js" &&
-
-python3 -m http.server $port --directory $destination
--- config/serve.sh
+++ config/serve.sh
@@ -1,12 +1,12 @@
 config=config/_default/hugo.yml
 destination=public
-port=80
+port=443
 
 rm -rf $destination &&
 
 mkdir $destination &&
 
-hugo server --bind="0.0.0.0" --buildFuture --cleanDestinationDir --config $config --disableFastRender --gc --ignoreCache --minify=false --noHTTPCache --port=$port --renderToDisk &&
+hugo server --bind="0.0.0.0" --buildFuture --cleanDestinationDir --config $config --disableFastRender --gc --ignoreCache --minify=false --noHTTPCache --port=$port --renderToDisk --tlsAuto &&
 
 rm -rf $destination
 rm -rf resources
