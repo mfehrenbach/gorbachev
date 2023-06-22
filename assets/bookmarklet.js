206b320d031bc8a95abe589afbe1062379e237f8
Michael Fehrenbach <mf@michaelfehrenbach.com>
2023-06-22 19:40:02 -0400

    Add/remove `head` elements as needed.

--- assets/bookmarklet.js
+++ assets/bookmarklet.js
@@ -5,13 +5,17 @@ javascript:(function(){
 	const df = 'https://daringfireball.net'
 
 	if (location.href.includes(df)) {
-		const metaViewport = `<meta content="initial-scale=1, width=device-width, viewport-fit=cover" name="viewport">`
-		const replacementStyle = `<link href="{{ $replacement.Permalink }}" rel="stylesheet">`
-
 		document.querySelectorAll('link[rel=stylesheet]').forEach(stylesheet => stylesheet.remove())
 
-		document.head.insertAdjacentHTML('beforeend', [metaViewport])
-		document.head.insertAdjacentHTML('beforeend', replacementStyle)
+		const metaViewport =     `<meta content="initial-scale=1, width=device-width, viewport-fit=cover" name="viewport">`
+		const replacementStyle = `<link href="{{ $replacement.Permalink }}" rel="stylesheet">`
+
+		if (document.head.hasAttribute('gorbachev')) {
+			document.head.insertAdjacentHTML('beforeend', replacementStyle)
+		} else {
+			document.head.insertAdjacentHTML('beforeend', [metaViewport, replacementStyle])
+			document.head.setAttribute('gorbachev', '')
+		}
 	} else {
 		location.href = df
 	}
