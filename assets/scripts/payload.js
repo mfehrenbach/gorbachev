a3e71520967713035fcaf381f8aa561cc61ae2a7
Michael Fehrenbach <mf@michaelfehrenbach.com>
2023-06-21 20:03:21 -0400

    Starting into page-reset JS.

--- assets/scripts/payload.js
+++ assets/scripts/payload.js
@@ -3,7 +3,10 @@ const df = 'https://daringfireball.net'
 
 
 const prepPage = () => {
-	alert('JS loaded!')
+	const metaViewport = `<meta content="initial-scale=1, width=device-width, viewport-fit=cover" name="viewport">`
+	document.head.insertAdjacentHTML('beforeend', metaViewport)
+
+	document.querySelectorAll('link[rel="stylesheet"]').forEach(stylesheet => stylesheet.remove())
 }
 
 
