c353077670c3ec77c096070c07f0b3fa88f3330f
Michael Fehrenbach <mf@michaelfehrenbach.com>
2023-06-26 19:24:18 -0400

    Update bookmarklet to flag Windows.

--- assets/bookmarklet.js
+++ assets/bookmarklet.js
@@ -2,7 +2,8 @@
 {{- $replacement = $replacement | toCSS (dict `transpiler` `dartsass`) -}}
 
 javascript:(function(){
-	const df = 'https://daringfireball.net'
+	const df =   'https://daringfireball.net'
+	const html = document.documentElement
 
 	if (location.href.includes(df)) {
 		document.querySelectorAll('link[rel=stylesheet]').forEach(stylesheet => stylesheet.remove())
@@ -10,11 +11,12 @@ javascript:(function(){
 		const metaViewport =     `<meta content="initial-scale=1, width=device-width, viewport-fit=cover" name="viewport">`
 		const replacementStyle = `<link href="{{ $replacement.Permalink }}" rel="stylesheet">`
 
-		if (document.head.hasAttribute('gorbachev')) {
+		if (html.classList.contains('gorbachev')) {
 			document.head.insertAdjacentHTML('beforeend', replacementStyle)
 		} else {
+			if (navigator.appVersion.includes("Win")) html.classList.add('windows')
 			document.head.insertAdjacentHTML('beforeend', [metaViewport, replacementStyle])
-			document.head.setAttribute('gorbachev', '')
+			html.classList.add('gorbachev')
 		}
 	} else {
 		location.href = df
