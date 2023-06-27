b45224c49274d7e660374b0be94e732d0b3b5be9
Michael Fehrenbach <mf@michaelfehrenbach.com>
2023-06-27 10:24:08 -0400

    Use internal IP for local `baseURL`.

--- config/serve.sh
+++ config/serve.sh
@@ -1,12 +1,13 @@
 config=config/_default/hugo.yml
 destination=_site
+ip=https://$(ipconfig getifaddr en1)
 port=443
 
 rm -rf $destination &&
 
 mkdir $destination &&
 
-hugo server --bind="0.0.0.0" --buildFuture --cleanDestinationDir --config $config  --destination $destination --disableFastRender --gc --ignoreCache --minify=false --noHTTPCache --port=$port --renderToDisk --tlsAuto &&
+hugo server --baseURL=$ip --bind="0.0.0.0" --buildFuture --cleanDestinationDir --config $config  --destination $destination --disableFastRender --gc --ignoreCache --minify=false --noHTTPCache --port=$port --renderToDisk --tlsAuto &&
 
 rm -rf $destination
 rm -rf resources
