aa80e28be14532fe6d6294f3b2b94b3a73f5c270
Michael Fehrenbach <mf@michaelfehrenbach.com>
2023-06-28 17:50:52 -0400

    Avoid `.Content` → `.FromString` dance.

--- assets/scripts/bookmarklet.js
+++ assets/scripts/bookmarklet.js
@@ -1,6 +1,5 @@
 {{- $payload := resources.Get `scripts/payload.js` -}}
-{{- $payload = $payload.Content -}}
-{{- $payload = $payload | resources.FromString `payload.js` -}}
+{{- $payload = $payload | resources.Copy `payload.js` -}}
 
 javascript:(function(){
 	const df = 'https://daringfireball.net'
