f3397eefcc35f3c2afbdc473b17e04ca16fef7a4
Michael Fehrenbach <mf@michaelfehrenbach.com>
2023-07-13 18:17:20 -0400

    Better IP get?

--- config/serve.sh
+++ config/serve.sh
@@ -1,13 +1,13 @@
 config=config/_default/hugo.yml
 destination=_site
-ip=https://$(ipconfig getifaddr en1)
+ip=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}')
 port=443
 
 rm -rf $destination &&
 
 mkdir $destination &&
 
-hugo server --baseURL=$ip --bind="0.0.0.0" --buildFuture --cleanDestinationDir --config $config  --destination $destination --disableFastRender --gc --ignoreCache --minify=false --noHTTPCache --port=$port --renderToDisk --tlsAuto &&
+hugo server --baseURL=$ip --bind="0.0.0.0" --buildFuture --cleanDestinationDir --config $config --destination $destination --disableFastRender --gc --ignoreCache --minify=false --noHTTPCache --port=$port --renderToDisk --tlsAuto &&
 
 rm -rf $destination
 rm -rf resources
